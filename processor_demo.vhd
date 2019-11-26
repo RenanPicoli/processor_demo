0b044addc02fa79d53be208ce26a29786a133a05
RenanPicoli <renanps123@gmail.com>
2019-11-25 21:45:58 -0300

    moved parallel_load_cache to the top level

--- processor_demo.vhd
+++ processor_demo.vhd
@@ -45,18 +45,40 @@ port(	mantissa: in array4(0 to 7);--digits encoded in 4 bits
 end component;
 
 component microprocessor
+generic (N: integer);--size in bits of data addresses 
 port (CLK: in std_logic;
 		rst: in std_logic;
 		data_memory_output: buffer std_logic_vector(31 downto 0);
 		instruction_addr: out std_logic_vector (31 downto 0);--AKA read address
+		-----ROM----------
 		ADDR_rom: out std_logic_vector(4 downto 0);--addr é endereço de byte, mas os Lsb são 00
-		Q_rom:	in std_logic_vector(31 downto 0)
+		Q_rom:	in std_logic_vector(31 downto 0);
+		-----RAM-----------
+		ADDR_ram: out std_logic_vector(N-1 downto 0);--addr é endereço de byte, mas os Lsb são 00
+		write_data_ram: out std_logic_vector(31 downto 0);
+		fill_cache_ram: out std_logic;
+		rden_ram: out std_logic;--habilita leitura
+		wren_ram: out std_logic;--habilita escrita
+		Q_ram:in std_logic_vector(31 downto 0)
 );
 end component;
 
 component mini_rom
 	port (--CLK: in std_logic;--borda de subida para escrita, se desativado, memória é lida
 			ADDR: in std_logic_vector(4 downto 0);--addr é endereço de byte, mas os Lsb são 00
+			Q:	out std_logic_vector(31 downto 0)
+			);
+end component;
+
+component parallel_load_cache
+	generic (N: integer);--size in bits of address 
+	port (CLK: in std_logic;--borda de subida para escrita, memória pode ser lida a qq momento desde que rden=1
+			ADDR: in std_logic_vector(N-1 downto 0);--addr é endereço de byte, mas os Lsb são 00
+			write_data: in std_logic_vector(31 downto 0);
+			parallel_write_data: in array32 (0 to 2**N-1);
+			fill_cache: in std_logic;
+			rden: in std_logic;--habilita leitura
+			wren: in std_logic;--habilita escrita
 			Q:	out std_logic_vector(31 downto 0)
 			);
 end component;
@@ -71,24 +93,54 @@ signal en_7seg: std_logic;
 
 signal CLK: std_logic := '0';
 signal count: std_logic_vector(29 downto 0) := (others=>'0');
-
+-----------signals for ROM interfacing---------------------
 signal instruction_memory_output: std_logic_vector(31 downto 0);
 signal instruction_memory_address: std_logic_vector(4 downto 0);
+-----------signals for RAM interfacing---------------------
+constant N: integer := 5;
+signal ram_clk: std_logic;--data memory clock signal
+signal ram_addr: std_logic_vector(N-1 downto 0);
+signal ram_write_data: std_logic_vector(31 downto 0);
+signal parallel_write_data: array32 (0 to 2**N-1);
+signal ram_fill_cache: std_logic;
+signal ram_rden: std_logic;
+signal ram_wren: std_logic;
+signal ram_Q: std_logic_vector(31 downto 0);
 
 	begin
 	
 	rom: mini_rom port map(	--CLK => CLK,
 									ADDR=> instruction_memory_address,
 									Q	 => instruction_memory_output
-	);	
+	);
 	
-	processor: microprocessor port map (
+	--MINHA ESTRATEGIA É EXECUTAR CÁLCULOS NA SUBIDA DE CLK E GRAVAR Na MEMÓRIA NA BORDA DE DESCIDA
+	ram_clk <= not CLK;											
+	ram: parallel_load_cache generic map (N => N)
+									port map(CLK	=> ram_clk,
+												ADDR	=> ram_addr,
+												write_data => ram_write_data,
+												parallel_write_data => parallel_write_data,
+												fill_cache => ram_fill_cache,
+												rden	=> ram_rden,
+												wren	=> ram_wren,
+												Q		=> ram_Q);
+	
+	processor: microprocessor
+	generic map (N => N)
+	port map (
 		CLK => CLK,
 		rst => rst,
 		data_memory_output => data_memory_output,
 		instruction_addr => instruction_addr,
 		ADDR_rom => instruction_memory_address,
-		Q_rom => instruction_memory_output
+		Q_rom => instruction_memory_output,
+		ADDR_ram => ram_addr,
+		write_data_ram => ram_write_data,
+		fill_cache_ram => ram_fill_cache,
+		rden_ram => ram_rden,
+		wren_ram => ram_wren,
+		Q_ram => ram_Q
 	);
 
 	converter: decimal_converter port map(
