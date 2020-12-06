1fba24bbf275684bdc8a54658a51d2ac49ade106
RenanPicoli <renanps123@gmail.com>
2020-12-06 14:41:54 -0300

    created a port for single coefficient reading; port renaming; created a process for single coefficient reading controlled by RDEN port

--- generic_coeffs_mem.vhd
+++ generic_coeffs_mem.vhd
@@ -19,9 +19,11 @@ generic	(N: natural; P: natural; Q: natural);--N address width in bits
 port(	D:	in std_logic_vector(31 downto 0);-- um coeficiente é carregado por vez
 		ADDR: in std_logic_vector(N-1 downto 0);--se ALTERAR P, Q PRECISA ALTERAR AQUI
 		RST:	in std_logic;--synchronous reset
+		RDEN:	in std_logic;--read enable
 		WREN:	in std_logic;--write enable
 		CLK:	in std_logic;
-		Q_coeffs:	out std_logic_vector(32*(P+Q+1)-1 downto 0)-- todos os coeficientes são lidos de uma vez
+		Q_coeffs: out std_logic_vector(31 downto 0);--single coefficient reading
+		all_coeffs:	out std_logic_vector(32*(P+Q+1)-1 downto 0)-- todos os coeficientes são lidos de uma vez
 );
 
 end generic_coeffs_mem;
@@ -58,7 +60,7 @@ begin
 
    process(CLK)
    begin
-
+	--processo de escrita
 	if (CLK'event and CLK = '1') then
 		if (RST='1') then
 --			possible_outputs <= (others=>(others=>'0'));
@@ -66,16 +68,24 @@ begin
 			possible_outputs(to_integer(unsigned(ADDR))) <= D;
 		end if;
 	end if;
-   end process;
 	
-	--leitura pode ser feita a qualquer momento
-	--OBS: MUST HAVE A LABEL!
+	--processo de leitura (um coeficiente de cada vez)
+	if (CLK'event and CLK = '1') then
+		if (RDEN ='1' and RST='0') then
+			Q_coeffs <= possible_outputs(to_integer(unsigned(ADDR)));
+		else
+			Q_coeffs <= (others=>'Z');
+		end if;
+	end if;
+   end process;	
+
+	--filtro tem acesso simultâneo a todos os coeficientes pela porta all_coeffs
 	coeffs_b: for i in 0 to P generate--coeficientes de x (b)
-		Q_coeffs((32*i+31) downto (32*i)) <= possible_outputs(i);
+		all_coeffs((32*i+31) downto (32*i)) <= possible_outputs(i);
 	end generate;
 	
 	coeffs_a: for j in 1 to Q generate--coeficientes de y (a)
-		Q_coeffs((32*(j+P)+31) downto (32*(j+P))) <= possible_outputs(j+P);
+		all_coeffs((32*(j+P)+31) downto (32*(j+P))) <= possible_outputs(j+P);
 	end generate;
 
 end behv;
