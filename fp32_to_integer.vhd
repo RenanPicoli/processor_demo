b9d087a9ea0ce2620054f4dc57ae621ba9838f17
RenanPicoli <renanps123@gmail.com>
2021-03-07 22:39:47 -0300

    fp32_to_integer: added eskeleton of floating point to integer conversion

--- fp32_to_integer.vhd
+++ fp32_to_integer.vhd
@@ -9,7 +9,7 @@ use ieee.std_logic_unsigned.all;
 
 use ieee.numeric_std.all;--to_integer
 use work.my_types.all;--array32
-use work.single_prec_type.all;--defines floating point single precision fields and constants
+use work.single_precision_type.all;--float--defines floating point single precision fields and constants
 
 ---------------------------------------------------
 
@@ -22,9 +22,31 @@ end fp32_to_integer;
 
 ---------------------------------------------------
 
-architecture behv of filter is
+architecture behv of fp32_to_integer is
 
-begin	
+--signals of floating point input
+signal sign: std_logic;--floating point sign bit, '1' means negative
+signal mantissa: std_logic_vector(22 downto 0);--mantissa plus implicit '1'
+signal extended_mantissa: std_logic_vector(23 downto 0);--mantissa plus implicit '1'
+signal exponent: std_logic_vector(7 downto 0);--BIASED exponent
+signal unbiased_exponent: std_logic_vector(7 downto 0);--exponent without bias
+
+--signals of integer output
+signal int_absolute: std_logic_vector(N-1 downto 0);
+signal int_output: std_logic_vector(N-1 downto 0);
+
+begin
+	sign <= fp_in(31);
+	mantissa <= fp_in(30 downto 8);
+	exponent <= fp_in(7 downto 0);
+	unbiased_exponent <= exponent - EXP_BIAS;
+	extended_mantissa <= '1' & mantissa;
+	
+	--int_absolute calculation
+	--shifts extended_mantissa the number of exponent without bias (*2^(EXP-bias))
+	
+	--output write
+	int_output <= ((not int_absolute)+'1')when sign='1' else int_absolute;
 
 end behv;
 
