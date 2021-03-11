4676dc9d4d91e6b6f4287bf77b3f407b06964f7d
RenanPicoli <renanps123@gmail.com>
2021-03-10 22:54:44 -0300

    var_shift: changed output size, using ror with shift as signed

--- var_shift.vhd
+++ var_shift.vhd
@@ -13,10 +13,10 @@ use ieee.numeric_std.all;--to_integer
 ---------------------------------------------------
 
 entity var_shift is
-generic	(N: natural; S: natural);--N: number of bits in output; S: number of bits in shift
+generic	(N: natural;O: natural; S: natural);--N: number of bits in input, O in output; S: number of bits in shift
 port(	input:in std_logic_vector(N-1 downto 0);--input vector that will be shifted
 		shift:in std_logic_vector(S-1 downto 0);--unsigned integer meaning number of shifts to left
-		output: out std_logic_vector(N-1 downto 0)--
+		output: out std_logic_vector(O-1 downto 0)--
 );
 end var_shift;
 
@@ -24,14 +24,14 @@ end var_shift;
 
 architecture behv of var_shift is
 
-type choices is array (0 to 2**S-1) of std_logic_vector(N-1 downto 0);
+type choices is array (0 to 2**S-1) of std_logic_vector(O-1 downto 0);
 signal possible_outputs: choices;
 
 begin
 	--implements all possible shifts at once, let the user select the appropriate
 
 	shifts: for i in 0 to 2**S-1 generate
-		possible_outputs(i) <= to_stdlogicvector(to_bitvector(input) rol to_integer(unsigned(shift)));
+		possible_outputs(i) <= to_stdlogicvector(to_bitvector(input) ror (N -  O) -  to_integer(signed(shift)))(O-1 downto 0);
 	end generate;
 
 	
