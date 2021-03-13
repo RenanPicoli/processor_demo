f2e94a00b3431b4402a3835409b44e90ac00b701
RenanPicoli <renanps123@gmail.com>
2021-03-13 19:49:09 -0300

    var_shift: added a port for overflag flag and signal containing dropped bits

--- var_shift.vhd
+++ var_shift.vhd
@@ -16,6 +16,7 @@ entity var_shift is
 generic	(N: natural;O: natural; S: natural);--N: number of bits in input, O in output; S: number of bits in shift
 port(	input:in std_logic_vector(N-1 downto 0);--input vector that will be shifted
 		shift:in std_logic_vector(S-1 downto 0);--signed integer: number of shifts to left (if positive)
+		overflow: out std_logic;-- there are ones dropped
 		output: out std_logic_vector(O-1 downto 0)--
 );
 end var_shift;
@@ -24,19 +25,22 @@ end var_shift;
 
 architecture behv of var_shift is
 
-type choices is array (-2**(S-1) to 2**(S-1)-1) of std_logic_vector(O-1 downto 0);
+type choices is array (-2**(S-1) to 2**(S-1)-1) of std_logic_vector(N-1 downto 0);
 signal possible_outputs: choices;
+signal output_dropped_bits: std_logic_vector(N-1 downto O);
 
 begin
 	--implements all possible shifts at once, let the user select the appropriate
 
 	shifts: for i in -2**(S-1) to 2**(S-1)-1 generate
-		possible_outputs(i) <= to_stdlogicvector(to_bitvector(input) srl ((N - O) - i))(O-1 downto 0);--shift right logic
+		possible_outputs(i) <= to_stdlogicvector(to_bitvector(input) srl ((N - O) - i));--shift right logic
 	end generate;
 
 	
 	--output write
-	output <= possible_outputs(to_integer(signed(shift)));
+	output_dropped_bits <= possible_outputs(to_integer(signed(shift)))(N-1 downto O);--for debugging ease
+	overflow <= '1' when output_dropped_bits /= "00000000" else '0';
+	output <= possible_outputs(to_integer(signed(shift)))(O-1 downto 0);
 
 end behv;
 
