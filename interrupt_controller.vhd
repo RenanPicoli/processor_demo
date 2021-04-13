0cc6bb93e3ab69bf067d82d05c811b88d8eb17a0
RenanPicoli <renanps123@gmail.com>
2021-04-12 23:55:42 -0300

    interrupt_controller: added address_decoder_register_map to handle acess to multiple irq controller registers, added ADDR port

--- interrupt_controller.vhd
+++ interrupt_controller.vhd
@@ -13,13 +13,14 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 
-use ieee.numeric_std.all;--to_integer
+use work.my_types.all;--array32
 
 ---------------------------------------------------
 
 entity interrupt_controller is
 generic	(L: natural);--L: number of IRQ lines
 port(	D: in std_logic_vector(31 downto 0);-- input: data to register write
+		ADDR: in std_logic_vector(1 downto 0);--address offset of registers relative to peripheral base address
 		CLK: in std_logic;-- input
 		RST: in std_logic;-- input
 		WREN: in std_logic;-- input
@@ -36,7 +37,36 @@ end interrupt_controller;
 ---------------------------------------------------
 
 architecture behv of interrupt_controller is
+	component address_decoder_register_map
+	--N: address width in bits
+	--boundaries: upper limits of each end (except the last, which is 2**N-1)
+	generic	(N: natural);
+	port(	ADDR: in std_logic_vector(N-1 downto 0);-- input
+			RDEN: in std_logic;-- input
+			WREN: in std_logic;-- input
+			WREN_OUT: out std_logic_vector;-- output
+			data_in: in array32;-- input: outputs of all peripheral/registers
+			data_out: out std_logic_vector(31 downto 0)-- data read
+	);
+	end component;
 	
+	signal irq_pend_Q: std_logic_vector(31 downto 0);
+	signal irq_pend_rden: std_logic;
+	signal irq_pend_wren: std_logic;-- not used, just to keep form
+		
+	signal iack_pend_Q: std_logic_vector(31 downto 0);
+	signal iack_pend_rden: std_logic;
+	signal iack_pend_wren: std_logic;-- not used, just to keep form
+		
+	signal iack_finished_Q: std_logic_vector(31 downto 0);
+	signal iack_finished_rden: std_logic;
+	signal iack_finished_wren: std_logic;-- not used, just to keep form
+	
+	signal all_registers_output: array32 (3 downto 0);
+	signal all_periphs_rden: std_logic_vector(3 downto 0);
+	signal address_decoder_wren: std_logic_vector(3 downto 0);
+	
+	------------signals for IRQ control-------------------	
 	signal IRQ_pend_out:		std_logic_vector(31 downto 0);-- status of all IRQs
 	signal IACK_pend_out:	std_logic_vector(31 downto 0);-- where the IACK must be sent when IACK_IN is asserted by processor
 	signal IACK_finished:	std_logic_vector(31 downto 0);	-- '1' when IACK_OUT is deasserted
@@ -46,12 +76,13 @@ architecture behv of interrupt_controller is
 begin
 	
 ---------------------------------- IRQ pending register ------------------------------------
+		irq_pend_wren <= address_decoder_wren(0);
 		irq_pending_i: for i in 0 to L-1 generate
-			irq_pending: process(RST,WREN,D,IRQ_pend_out,IRQ_IN)
+			irq_pending: process(RST,irq_pend_wren,D,IRQ_pend_out,IRQ_IN)
 			begin
 				if(RST='1') then
 					IRQ_pend_out(i) <= '0';
-				elsif (WREN='1') then --software writes '0' to clear pending bits, '1' is dont't care
+				elsif (irq_pend_wren='1') then --software writes '0' to clear pending bits, '1' is dont't care
 					IRQ_pend_out(i) <= IRQ_pend_out(i) and D(i);
 				elsif (rising_edge(IRQ_IN(i))) then -- IRQ assertion
 					IRQ_pend_out(i) <= '1';
@@ -60,6 +91,9 @@ begin
 		end generate irq_pending_i;		
 		IRQ_pend_out(31 downto L) <= (others => '0');--unused bits receive '0'
 		
+		
+---------------------------------- IACK pending register ------------------------------------
+		iack_pend_wren <= address_decoder_wren(1);
 		--detects where the IACK must be sent when IACK_IN is asserted by processor
 		iack_pending_i: for i in 0 to L-1 generate
 			iack_pending: process(RST,IACK_finished,IRQ_pend_out)
@@ -75,6 +109,9 @@ begin
 		end generate iack_pending_i;
 		IACK_pend_out(31 downto L) <= (others => '0');--unused bits receive '0'
 		
+
+---------------------------------- IACK finished register ------------------------------------
+		iack_finished_wren <= address_decoder_wren(2);
 		iack_finished_i: for i in 0 to L-1 generate
 			process (RST,IACK_OUT,IRQ_pend_out,CLK)
 			begin
@@ -104,6 +141,21 @@ begin
 		irq <= tmp(L-1);
 		IRQ_OUT <= irq;
 		output <= IRQ_pend_out;
+		
+-------------------------- address decoder ---------------------------------------------------
+	--addr 00: irq_pend
+	--addr 01: iack_pend
+	--addr 10: iack_finished
+	all_registers_output <= (0=> irq_pend_Q,1=> iack_pend_Q,2=> iack_finished_Q);
+	decoder: address_decoder_register_map
+	generic map(N => 2)
+	port map(ADDR => ADDR,
+				RDEN => RDEN,
+				WREN => WREN,
+				data_in => all_registers_output,
+				WREN_OUT => address_decoder_wren,
+				data_out => output
+	);
 end behv;
 
 ---------------------------------------------------------------------------------------------
