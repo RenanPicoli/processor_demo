ca4ad032c99454f1a37f16a272409cecdb7b32f0
RenanPicoli <renanps123@gmail.com>
2021-04-12 22:46:47 -0300

    interrupt_controller: completely refactored. Tested and approved. Now it has 3 registers: for IRQ pending, IACK pending and IACK finished. Supports up to 32 IRQ lines. IACK is now stored internally while waiting for CPU to send IACK.

--- interrupt_controller.vhd
+++ interrupt_controller.vhd
@@ -36,61 +36,64 @@ end interrupt_controller;
 ---------------------------------------------------
 
 architecture behv of interrupt_controller is
-
-	component d_flip_flop
-		port (D:	in std_logic_vector(31 downto 0);--only bit zeroed have effect, '1' is ignored
-				rst:	in std_logic;--synchronous reset
-				ENA:	in std_logic:='1';--enables writes
-				CLK:in std_logic;
-				Q:	out std_logic_vector(31 downto 0)  
-				);
-	end component;
-
-	signal IRQ_pend: 			std_logic_vector(31 downto 0);-- data to be written: from processor or IRQ lines
+	
 	signal IRQ_pend_out:		std_logic_vector(31 downto 0);-- status of all IRQs
-	signal previous_IRQ_pend:		std_logic_vector(31 downto 0);-- previous vlue of status of all IRQs
-	signal IRQ_IN_extended: std_logic_vector(31 downto 0);-- zero-extension of IRQ lines
+	signal IACK_pend_out:	std_logic_vector(31 downto 0);-- where the IACK must be sent when IACK_IN is asserted by processor
+	signal IACK_finished:	std_logic_vector(31 downto 0);	-- '1' when IACK_OUT is deasserted
 	signal irq:					std_logic;
-	signal tmp:					std_logic_vector(L-1 downto 0);
-	signal cleared: 			std_logic_vector(31 downto 0);-- '1' in positions where the pending bit went from '1' to '0'.
-	signal clr_rst:			std_logic := '0';
-	signal clr_set:			std_logic := '0';
+	signal tmp:					std_logic_vector(31 downto 0);
 
 begin
 	
 ---------------------------------- IRQ pending register ------------------------------------
-		-- status ('1' if pending) of all IRQ lines
-		irq_pending: d_flip_flop port map(	D => IRQ_pend,
-														RST=> RST,--resets all previous history of input signal
-														CLK=> CLK,--sampling clock
-														Q=> IRQ_pend_out
-														);
+		irq_pending_i: for i in 0 to L-1 generate
+			irq_pending: process(RST,WREN,D,IRQ_pend_out,IRQ_IN)
+			begin
+				if(RST='1') then
+					IRQ_pend_out(i) <= '0';
+				elsif (WREN='1') then --software writes '0' to clear pending bits, '1' is dont't care
+					IRQ_pend_out(i) <= IRQ_pend_out(i) and D(i);
+				elsif (rising_edge(IRQ_IN(i))) then -- IRQ assertion
+					IRQ_pend_out(i) <= '1';
+				end if;
+			end process;
+		end generate irq_pending_i;		
+		IRQ_pend_out(31 downto L) <= (others => '0');--unused bits receive '0'
 		
-		IRQ_IN_extended <= (31 downto L => '0') & IRQ_IN;
-		-- based on IRQ lines and signal WREN, decides what to write in IRQ_pend, BEFORE irq_pend_out UPDATE
-		IRQ_pend_write: process(WREN, IRQ_IN_extended, D, CLK)
-		begin
-			if (WREN='1') then
-				IRQ_pend	<= D and IRQ_IN_extended;
-			else
-				IRQ_pend <= IRQ_IN_extended;
-			end if;
-		end process;
+		--detects where the IACK must be sent when IACK_IN is asserted by processor
+		iack_pending_i: for i in 0 to L-1 generate
+			iack_pending: process(RST,IACK_finished,IRQ_pend_out)
+			begin
+				if(RST='1') then
+					IACK_pend_out(i) <= '0';
+				elsif (IACK_finished(i)='1') then --software sends IACK
+					IACK_pend_out(i) <= '0';
+				elsif (falling_edge(IRQ_pend_out(i))) then -- IRQ assertion
+					IACK_pend_out(i) <= '1';
+				end if;
+			end process;
+		end generate iack_pending_i;
+		IACK_pend_out(31 downto L) <= (others => '0');--unused bits receive '0'
 		
-		cleared <= (not IRQ_pend_out) and previous_IRQ_pend;--identifica com '1' as posições que foram de '1' a '0'
-		
-		process (CLK)
-		begin
-			if (rising_edge(CLK)) then
-				previous_IRQ_pend <= IRQ_pend_out;
-			end if;
-		end process;
+		iack_finished_i: for i in 0 to L-1 generate
+			process (RST,IACK_OUT,IRQ_pend_out,CLK)
+			begin
+				if(RST='1') then
+					IACK_finished(i) <= '0';
+				elsif (IRQ_pend_out(i)='1') then
+					IACK_finished(i) <= '0';
+				elsif (falling_edge(IACK_OUT(i))) then-- asserts previous value
+					IACK_finished(i) <= '1';
+				end if;
+			end process;
+		end generate iack_finished_i;
+		IACK_finished(31 downto L) <= (others => '0');--unused bits receive '0'
 		
 		-- AFTER irq_pend_out UPDATE
 		--é necessário que o software zere os bits das IRQ atendidas e
 		--DEPOIS envie o IACK.
 		iack_out_write: for i in 0 to L-1 generate
-					IACK_OUT(i) <= IACK_IN and cleared(i);
+					IACK_OUT(i) <= IACK_IN and IACK_pend_out(i);
 		end generate;
 
 		-- AFTER irq_pend_out UPDATE
