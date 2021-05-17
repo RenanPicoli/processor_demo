8062b3b7468470f7425510e06ed004d20a2a98be
RenanPicoli <renanps123@gmail.com>
2021-05-17 19:56:52 -0300

    testbench: updated code to emulate interaction with a smaller version of sram (tb_sram)

--- testbench.vhd
+++ testbench.vhd
@@ -1,3 +1,5 @@
+--This testbench aims to emulate the behaviour of on-board SRAM
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
@@ -13,123 +15,138 @@ entity testbench is
 end testbench;
 
 architecture test of testbench is
--- "Time" that will elapse between test vectors we submit to the component.
-constant TIME_DELTA : time := 44 us;
-constant fs : integer := 22050;--frequência de amostragem do filtro
+
+--emulates 1/16 of the onboard sram (65536 16bit words)
+component tb_sram
+	PORT
+	(
+		address	: IN STD_LOGIC_VECTOR (15 DOWNTO 0);
+		clock		: IN STD_LOGIC  := '1';
+		data		: IN STD_LOGIC_VECTOR (15 DOWNTO 0);
+		wren		: IN STD_LOGIC ;
+		q		: OUT STD_LOGIC_VECTOR (15 DOWNTO 0)
+	);
+end component;
+
+component i2c_slave
+	port (
+			D: in std_logic_vector(31 downto 0);--for register write
+			ADDR: in std_logic_vector(1 downto 0);--address offset of registers relative to peripheral base address
+			CLK: in std_logic;--for register read/write, also used to generate SCL
+			RST: in std_logic;--reset
+			WREN: in std_logic;--enables register write
+			RDEN: in std_logic;--enables register read
+			IACK: in std_logic;--interrupt acknowledgement
+			Q: out std_logic_vector(31 downto 0);--for register read
+			IRQ: out std_logic;--interrupt request
+			SDA: inout std_logic;--open drain data line
+			SCL: inout std_logic --open drain clock line
+	);
+end component;
 
 --reset duration must be long enough to be perceived by the slowest clock (filter clock, both polarities)
 constant TIME_RST : time := 50 us;
+-- internal clock period.
+constant TIME_DELTA : time := 20 ns;
 
 signal  	CLK_IN:std_logic;--50MHz
 signal	rst: std_logic;
-signal  	data_in:std_logic_vector(31 downto 0) := (others => '0');
-signal  	desired:std_logic_vector(31 downto 0) := (others => '0');--desired response (response of unknown system)
 signal	data_out:std_logic_vector(31 downto 0);
-signal	instruction_addr:std_logic_vector(31 downto 0);
+
+--sram interface
+signal	ram_CLK: std_logic;--needed for conversion between sram and altsyncram (sram doesn't have clock)
+signal	ram_wren: std_logic;--needed for conversion between sram and altsyncram write enable pins
+signal	ram_Q: std_logic_vector(15 downto 0);--needed for conversion between sram and altsyncram data output pins
+
+signal 	sram_WE_n: std_logic;--write enable ACTIVE LOW
+signal	sram_IO: std_logic_vector(15 downto 0);--sram data; input because we'll only read
+signal	sram_ADDR: std_logic_vector(19 downto 0);--ADDR for SRAM
+
+--i2c interface
+signal	I2C_SDAT: std_logic;--I2C SDA
+signal	I2C_SCLK: std_logic;--I2C SCL
+
+--I2S/codec
+signal	MCLK: std_logic;-- master clock output for audio codec (12MHz)
+signal	AUD_BCLK: std_logic;--SCK aka BCLK_IN
+signal	AUD_DACDAT: std_logic;--DACDAT aka SD
+signal	AUD_DACLRCK: std_logic;--DACLRCK aka WS
+
+--GPIO/EX_IO interfaces
+signal	GPIO: std_logic_vector(35 downto 0);
+signal	EX_IO:std_logic_vector(6 downto 0);
+
+--i2c slave signals
+signal D_slv: std_logic_vector(31 downto 0);--for register write
+signal ADDR_slv: std_logic_vector(1 downto 0);--address offset of registers relative to peripheral base address
+signal WREN_slv: std_logic;--enables register write
+signal RDEN_slv: std_logic;--enables register read
+constant RW_bit: std_logic:='0';-- 1 read mode; 0 write mode
 
 signal	instruction_number: natural := 0;-- number of the instruction being executed
 
-signal	filter_CLK: std_logic := '0';-- to keep in sync with filter clock generated with PLL
+signal	filter_CLK: std_logic;--filter clock generated with PLL
+signal	filter_rst: std_logic;
 --signal	alternative_filter_CLK: std_logic := '0';-- to keep in sync with filter clock generated with PLL
-
-constant c_WIDTH : natural := 4;
-file 		input_file: text;-- open read_mode;--estrutura representando arquivo de entrada de dados
-file 		desired_file: text;-- open read_mode;--estrutura representando arquivo de entrada de dados
-file 		output_file: text;-- open write_mode;--estrutura representando arquivo de saída de dados
-
-constant COUNT_MAX: integer := 
-integer(floor(real(real(fs)*real(TIME_DELTA/1 us)/1000000.0)/real(2.0*(1.0-real(fs)*real(TIME_DELTA/1 us)/1000000.0))));
-
-constant FILTER_CLK_SEMIPERIOD: time := 22_675_736.961 ps;--maximum precision allowed by vhdl would be fs, but constant wouldnt fit an integer
 
 begin
 
+	I2C_SDAT <= 'H';--pull up resistor on-board
+
 	DUT: entity work.processor_demo
-	port map(CLK_IN 	=> CLK_IN,
-				rst	 	=> rst,
-				data_in 	=> data_in,
-				desired	=> desired,
-				filter_CLK_out=>filter_CLK,--filter clock: used as port so the testbench can synchronize sample presenting
-				data_out	=> data_out,
-				instruction_addr=>instruction_addr
+	port map(
+		CLK_IN => CLK_IN,--50MHz input
+		rst => rst,
+		data_out => data_out,--filter output (encoded in IEEE 754 single precision)
+		--I2C
+		I2C_SDAT => I2C_SDAT,--I2C SDA
+		I2C_SCLK => I2C_SCLK,--I2C SCL
+		--I2S/codec
+		MCLK => MCLK,-- master clock output for audio codec (12MHz)
+		AUD_BCLK => AUD_BCLK,--SCK aka BCLK_IN
+		AUD_DACDAT => AUD_DACDAT,--DACDAT aka SD
+		AUD_DACLRCK => AUD_DACLRCK,--DACLRCK aka WS
+		--SRAM
+		sram_IO => sram_IO,--sram data; input because we'll only read
+		sram_ADDR => sram_ADDR,--ADDR for SRAM
+		sram_CE_n => open,--chip enable, active LOW
+		sram_OE_n => open,--output enable, active LOW
+		sram_WE_n => sram_WE_n,--write enable, active LOW, HIGH enables reading
+		sram_UB_n => open,--upper IO byte access, active LOW
+		sram_LB_n => open, --lower	IO byte access, active LOW
+		--GREEN LEDS
+		LEDG => open,
+		--RED LEDS
+		LEDR => open,
+		--GPIO 14 PINS
+		EX_IO => EX_IO,
+		--GPIO 40 PINS
+		GPIO => GPIO
 	);
 	
 	--calculate number of instruction being executed
-	instruction_number <= to_integer(unsigned(instruction_addr))/4;
+	instruction_number <= to_integer(unsigned(GPIO(7 downto 0)));--row number of mini_rom, starting from 0
+	filter_CLK <= GPIO(14);
+	filter_rst <= GPIO(15);
 	
-	-----------------------------------------------------------
-	--	this process reads a file vector, loads its vectors,
-	--	passes them to the DUT and checks the result.
-	-----------------------------------------------------------
-	reading_process: process--parses input text file
-		variable v_space: character;--stores the white space used to separate 2 arguments
-		variable v_A: std_logic_vector(31 downto 0);--input of filter
-		variable v_B: std_logic_vector(31 downto 0);--desired response
-		variable v_iline_A: line;
-		variable v_iline_B: line;
-		
-		variable count: integer := 0;-- para sincronização da apresentação de amostras
-		
-	begin
-		file_open(input_file,"input_vectors.txt",read_mode);--PRECISA FICAR NA PASTA simulation/modelsim
-		file_open(desired_file,"desired_vectors.txt",read_mode);--PRECISA FICAR NA PASTA simulation/modelsim
-		
-		wait for TIME_RST;--wait until reset finishes
---		wait until filter_CLK ='1';-- waits until the first rising edge after reset
---		wait for (TIME_DELTA/2);-- additional delay (rising edge of sampling will be in the middle of sample)
-		wait until filter_CLK ='0';-- waits for first falling EDGE after reset
-		
-		while not endfile(input_file) loop
-			readline(input_file,v_iline_A);--lê uma linha do arquivo de entradas
-			hread(v_iline_A,v_A);
---			read(v_iline,v_space);
---			hread(v_iline,v_B);
-			
-			data_in <= v_A;-- assigns input to filter
-			
-			readline(desired_file,v_iline_B);--lê uma linha do arquivo de resposta desejada
-			hread(v_iline_B,v_B);
-			desired <= v_B;-- assigns desired response to the algorithm
-			
-			-- IMPORTANTE: CONVERSÃO DE TEMPO PARA REAL
-			-- se TIME_DELTA em ms, use 1000 e 1 ms
-			-- se TIME_DELTA em us, use 1000000 e 1 us
-			-- se TIME_DELTA em ns, use 1000000000 e 1 ns
-			-- se TIME_DELTA em ps, use 1000000000000 e 1 ps
-			if (count = COUNT_MAX) then
-				wait until filter_CLK ='1';-- waits until the first rising edge occurs
-				wait for (TIME_DELTA/2);-- reestabelece o devido delay entre amostras e clock de amostragem
-			else
-				if (count = COUNT_MAX + 1) then
-					count := 0;--variable assignment takes place immediately
-				end if;
-				wait for TIME_DELTA;-- usual delay between 2 samples
-			end if;
-			count := count + 1;--variable assignment takes place immediately
-		end loop;
-		
-		file_close(input_file);
-
-		wait; --?
-	end process;
+	--wren: active HIGH
+	--sram_WE_n: active LOW
+	ram_wren <= not sram_WE_n;
 	
-	--reads adaptive filter response
-	write_proc: process(data_out, filter_CLK)--writing output file every time data_out changes introduces spurious pulses
-		variable v_oline: line;
-		variable v_C: std_logic_vector(31 downto 0);--data to be written
-	begin
-		if (filter_CLK'event and filter_CLK='0') then-- falling_edge(filter_CLK): when outputs are sampled in filter and xN
-			file_open(output_file,"output_vectors.txt",append_mode);--PRECISA FICAR NA PASTA simulation/modelsim
-			
-			v_C := data_out;
-			hwrite(v_oline, v_C);--write values in hex notation
---			write(v_oline,string'(" "));
---			write(v_oline,time'image(now));
-			writeline(output_file, v_oline);
-				
-			file_close(output_file);
-		end if;
-	end process;
+	ram: tb_sram
+	port map
+	(
+		--trick: in real SRAM, bit 19 divides upper and lower halfs, in tb_sram this is done by bit 15
+		address	=> sram_ADDR(19) & sram_ADDR(14 downto 0),
+		clock		=> ram_CLK,--because address is updated at rising_edge of CLK_IN in my system
+		data		=> (others=>'0'),--data for write, but I will only read
+		wren		=> ram_wren,--active HIGH
+		q			=> ram_Q
+	);
+	--this delay should permit our ram to update its response correctly (valid data when CLK_IN goes low)
+	ram_CLK <= transport CLK_IN after 1 ns;
+	--sram_IO <= transport ram_Q after 9 ns;--emulates delay in sram response (less than 10 ns)
+	sram_IO <= ram_Q;
 	
 	clock: process--50MHz input clock
 	begin
@@ -139,14 +156,44 @@ begin
 		wait for 10 ns;
 	end process clock;
 	
---	filter_clock: process--22050Hz sampling clock
---	begin
---		alternative_filter_CLK <= '0';
---		wait for FILTER_CLK_SEMIPERIOD;
---		alternative_filter_CLK <= '1';
---		wait for FILTER_CLK_SEMIPERIOD;
---	end process filter_clock;
-	
 	rst <= '1', '0' after TIME_RST;--reset must be long enough to be perceived by the slowest clock (fifo)
+	
+	slave: i2c_slave
+	port map(D 		=> D_slv,
+				CLK	=> CLK_IN,
+				ADDR 	=> ADDR_slv,
+				RST	=>	rst,
+				WREN	=> WREN_slv,
+				RDEN	=>	RDEN_slv,
+				IACK	=> '0',
+				Q		=>	open,
+				IRQ	=>	open,
+				SDA	=>	I2C_SDAT,
+				SCL	=>	I2C_SCLK
+	);
+	
+	
+	slave_setup:process
+	begin
+		wait for TIME_RST;--+TIME_DELTA;
+		wait until CLK_IN='0';
+		--zeroes & WORDS & OADDR & R/W(must store RW bit sent by master; 1 read mode; 0 write mode)
+		ADDR_slv <= "00";--CR address
+		D_slv <= (31 downto 10 =>'0') & "01" & "0011010" & 'X';--WORDS: 01 (2 words); OADDR: 0011010
+		WREN_slv <= '1';
+		wait for TIME_DELTA;
+		
+		ADDR_slv <= "01";--DR address
+		--bits 7:0 data received or to be read by master	
+		D_slv <= x"0000_00A4";-- data to be read by master
+		WREN_slv <= '1';
+		wait for TIME_DELTA;
+
+		ADDR_slv<="11";--invalid address
+		D_slv<=(others=>'0');
+		WREN_slv <= '0';
+		wait for TIME_DELTA;
+		wait;--process executes once
+	end process slave_setup;
 	
 end architecture test;
