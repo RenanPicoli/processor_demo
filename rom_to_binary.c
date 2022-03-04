97b701076770556ad73a2a75fa085c15715a8992
RenanPicoli <renanps123@gmail.com>
2022-03-04 11:29:32 -0300

    Added rom_to_binary.c to compile assembly from mini_rom.vhd to generate a file for SRAM programming

---
+++ rom_to_binary.c
@@ -0,0 +1,168 @@
+/*
+	Generates binary files from vhdl code of mini_rom.vhd for SRAM programming
+	Generates a single file with 512Kx16bit data refering to data_in (input_vectors.txt) and 512Kx16 bit refering to desired values (desired_vectors.txt)
+	Values are 32 bit wide, but LSB are stored in address 2n, and MSB are stored in addr 2n+1
+	Example: data_in = 0x368F75BF
+	addr[0]= 0x75BF
+	addr[1]= 0x368F
+	This file will fill completely DE2-115 SRAM as I move the assembly to that memory
+	It will be downloaded using DE2 Control Panel
+*/
+//for printf, fopen, fgets, fwrite, etc.
+#include <stdio.h>
+
+//for malloc
+#include <stdlib.h>
+
+//for UINT_MAX
+#include <limits.h>
+
+//for strlen, strcpy, strncpy
+#include <string.h>
+
+//number of instructions to store
+//256 = 0x100
+#define N_INSTR 256
+
+//maximum length of line in text files
+#define MAX_STR_LENGTH 200
+
+//for the opcode dictionary
+typedef struct
+{
+	char name[11];//up to 10 chars and one null byte
+	char binary_string[7];//string containing up to 6 chars in {'0','1'} and one null byte
+}node;
+
+//prototype of function to convert hex strings to unsigned int
+unsigned int hex2uint(char* str);
+
+int main(void){
+	FILE *fp;//file pointer for read
+	FILE *fp_types;//file pointer for read, contains the opcodes and codes for registers
+	FILE *of;//file pointer for outputfile
+/*
+	of=fopen("./sram_file_experimental.bin","wb");//NOTE THE 'b' in "wb" mode: opens a BINARY file for write, otherwise, will line ending chars
+	if(of==NULL){
+		printf("Erro ao criar o arquivo de saída!\n");
+		return 1;
+	}
+*/
+	node* dictionary=NULL;
+	char* tmp_str=calloc(MAX_STR_LENGTH,sizeof(char));//temporary string to store lines read from files
+	char* instruction_str=calloc(MAX_STR_LENGTH,sizeof(char));//temporary string to store lines read from files
+	char* comment_str=calloc(MAX_STR_LENGTH,sizeof(char));//temporary string to store single line comment read from files
+	unsigned int *ptr=malloc(1*sizeof(unsigned int));//temporary pointer to store instruction generated from a single line of fp
+	char* s0=calloc(MAX_STR_LENGTH,sizeof(char));//temporary string to store lines read from files
+	char* s1=calloc(MAX_STR_LENGTH,sizeof(char));//temporary string to store lines read from files
+	char* s2=calloc(MAX_STR_LENGTH,sizeof(char));//temporary string to store lines read from files
+	char* s3=calloc(MAX_STR_LENGTH,sizeof(char));//temporary string to store lines read from files
+	char* s4=calloc(MAX_STR_LENGTH,sizeof(char));//temporary string to store lines read from files
+	char* s5=calloc(MAX_STR_LENGTH,sizeof(char));//temporary string to store lines read from files
+	if(ptr==NULL){
+		printf("Erro ao alocar a memória para o ponteiro ptr\n");
+		return 5;
+	}
+	if(tmp_str==NULL||instruction_str==NULL||comment_str==NULL){
+		printf("Erro ao alocar a memória para o ponteiro tmp_str ou instruction_str ou comment_str\n");
+		return 6;
+	}
+	fp=fopen("./microprocessor/mini_rom.vhd","r");//instructions will only be read
+	fp_types=fopen("./microprocessor/my_types.vhd","r");//instructions will only be read
+	if(fp==NULL){
+		printf("Erro ao ler o arquivo mini_rom.vhd\n");
+		return 2;
+	}
+	if(fp_types==NULL){
+		printf("Erro ao ler o arquivo my_types.vhd\n");
+		return 3;
+	}
+	int sscanf_retval=0;//to store sscanf return value to check if it was successful
+	int fwrite_retval=0;//to store fwrite return value to check if it was successful
+
+	printf("Parsing my_types.vhd\n");
+	int j=0;
+	while (!feof(fp_types)){
+		fgets((char*)tmp_str,MAX_STR_LENGTH,fp_types);//reads a single line of fp_types, terminated with '\n', expects at most 199 chars
+		sscanf_retval=sscanf(tmp_str,"constant %[a-zA-Z0-9&\"_ ] : %*[a-zA-Z0-9&\"_() ] := \"%[01]\" ;",s0,s1);
+		if(sscanf_retval>1){
+			//printf("Opcode j=%d: %s := %s\n",j,s0,s1);
+			dictionary = realloc(dictionary,(j+1)*sizeof(node));
+			strncpy(dictionary[j].name,s0,11);
+			strncpy(dictionary[j].binary_string,s1,7);
+			printf("Opcode j=%d: %s := %s\n",j,dictionary[j].name,dictionary[j].binary_string);
+			j++;
+		}
+		fgetc(fp_types);//reads and discards the newline
+	}
+	printf("my_types.vhd parsed!\n");
+
+	printf("Parsing mini_rom.vhd\n");
+	int i=0;
+	while (!feof(fp)){
+		fgets((char*)tmp_str,MAX_STR_LENGTH,fp);//reads a single line of fp, terminated with '\n', expects at most 199 chars
+		sscanf_retval=sscanf(tmp_str,"%d => %[a-zA-Z0-9&\"_ ],%*s",&i,instruction_str);//reads a single line of fp, with no spaces, terminated with '\n', expects 8 hexadecimal digits (32bit data)
+		//sscanf_retval=sscanf(tmp_str,"%d => %s & %s & %s & %s & %s & %s , %*s",&i,s0,s1,s2,s3,s4,s5);//reads a single line of fp, with no spaces, terminated with '\n', expects 8 hexadecimal digits (32bit data)
+		if(sscanf_retval>1){
+			//printf("Instrução i=%d: %s %s %s %s %s %s\n",i,s0,s1,s2,s3,s4,s5);
+			printf("Instrução i=%d: %s\n",i,instruction_str);
+		}
+		fgetc(fp);//reads and discards the newline
+		//tmp_str[8]='\0';
+		//printf("i:%d tmp_str:%s ",i,tmp_str);
+		// *ptr=hex2uint(tmp_str);//stores in ptr the binary representation of the float represented in string tmp_str
+		//printf("*ptr:%X\n",*ptr);
+		/*
+		if(*ptr==UINT_MAX){//detects error of conversion (likely invalid char)
+			printf("Erro: char invalido na linha %d : \"%s\" de input_vectors.txt\n",i,tmp_str);
+			return 4;
+		}
+		fwrite_retval=fwrite(ptr,sizeof(unsigned int),1,of);//writes the 32bit unsigned int
+		if(fwrite_retval!=1){
+			printf("Erro de fwrite!\ns");
+			return 8;
+		}
+		*/
+	}
+	printf("mini_rom.vhd parsed!\n");
+	fclose(fp);//closes mini_rom.vhd
+	fclose(fp_types);//closes my_types.vhd
+
+	//close output file
+	fclose(of);
+	//printf("Arquivo \"sram_file\" gerado com sucesso!\n");
+	free(dictionary);
+	free(tmp_str);
+	free(instruction_str);
+	free(comment_str);
+	free(ptr);
+	free(s0);
+	free(s1);
+	free(s2);
+	free(s3);
+	free(s4);
+	free(s5);
+	return 0;
+}
+
+//function to convert hex strings to unsigned int
+unsigned int hex2uint(char* str){
+	unsigned int retval=0;
+	int L=strlen(str);//string length, does not count the '\0'
+	
+	for(int i=0;i <= L-1;i++){//goes from most significant digit to least significant (left to right)
+		//printf("hex2uint: i: %d L:%d char: %c\n",i,L,str[i]);
+		if(str[i] >= '0' && str[i] <= '9'){//detects numeric char
+			retval = 16*retval + (str[i]-'0');
+		}else if(str[i] >= 'A' && str[i] <= 'F'){//detects alfabetic char (upper case)
+			retval = 16*retval + (str[i]-'A'+10);
+		}else if(str[i] >= 'a' && str[i] <= 'f'){//detects alfabetic char (lower case)
+			retval = 16*retval + (str[i]-'a'+10);
+		}else{//invalid char detected
+			retval = UINT_MAX;
+			return retval;
+		}
+	}
+	//printf(" hex returns: %X ",retval);
+	return retval;
+}
