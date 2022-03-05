deb8072adc5410632d9faf64c2b3a123a7d49c39
RenanPicoli <renanps123@gmail.com>
2022-03-05 19:54:33 -0300

    pll_dbg_uproc, SDC: clock for debugging was changed to 48MHz

--- pll_dbg_uproc.vhd
+++ pll_dbg_uproc.vhd
@@ -159,7 +159,7 @@ BEGIN
 		bandwidth_type => "AUTO",
 		clk0_divide_by => 25,
 		clk0_duty_cycle => 50,
-		clk0_multiply_by => 6,
+		clk0_multiply_by => 24,
 		clk0_phase_shift => "0",
 		clk1_divide_by => 25,
 		clk1_duty_cycle => 50,
@@ -256,7 +256,7 @@ END SYN;
 -- Retrieval info: PRIVATE: DUTY_CYCLE0 STRING "50.00000000"
 -- Retrieval info: PRIVATE: DUTY_CYCLE1 STRING "50.00000000"
 -- Retrieval info: PRIVATE: DUTY_CYCLE2 STRING "50.00000000"
--- Retrieval info: PRIVATE: EFF_OUTPUT_FREQ_VALUE0 STRING "12.000000"
+-- Retrieval info: PRIVATE: EFF_OUTPUT_FREQ_VALUE0 STRING "48.000000"
 -- Retrieval info: PRIVATE: EFF_OUTPUT_FREQ_VALUE1 STRING "4.000000"
 -- Retrieval info: PRIVATE: EFF_OUTPUT_FREQ_VALUE2 STRING "16.000000"
 -- Retrieval info: PRIVATE: EXPLICIT_SWITCHOVER_COUNTER STRING "0"
@@ -289,7 +289,7 @@ END SYN;
 -- Retrieval info: PRIVATE: MULT_FACTOR1 NUMERIC "1"
 -- Retrieval info: PRIVATE: MULT_FACTOR2 NUMERIC "1"
 -- Retrieval info: PRIVATE: NORMAL_MODE_RADIO STRING "1"
--- Retrieval info: PRIVATE: OUTPUT_FREQ0 STRING "12.00000000"
+-- Retrieval info: PRIVATE: OUTPUT_FREQ0 STRING "48.00000000"
 -- Retrieval info: PRIVATE: OUTPUT_FREQ1 STRING "4.00000000"
 -- Retrieval info: PRIVATE: OUTPUT_FREQ2 STRING "16.00000000"
 -- Retrieval info: PRIVATE: OUTPUT_FREQ_MODE0 STRING "1"
@@ -346,7 +346,7 @@ END SYN;
 -- Retrieval info: CONSTANT: BANDWIDTH_TYPE STRING "AUTO"
 -- Retrieval info: CONSTANT: CLK0_DIVIDE_BY NUMERIC "25"
 -- Retrieval info: CONSTANT: CLK0_DUTY_CYCLE NUMERIC "50"
--- Retrieval info: CONSTANT: CLK0_MULTIPLY_BY NUMERIC "6"
+-- Retrieval info: CONSTANT: CLK0_MULTIPLY_BY NUMERIC "24"
 -- Retrieval info: CONSTANT: CLK0_PHASE_SHIFT STRING "0"
 -- Retrieval info: CONSTANT: CLK1_DIVIDE_BY NUMERIC "25"
 -- Retrieval info: CONSTANT: CLK1_DUTY_CYCLE NUMERIC "50"
