ceb129f635c11f5a56823ffc2b570b098e055f01
RenanPicoli <renanps123@gmail.com>
2022-07-27 17:41:29 -0300

    async_sram.vhd: using a generic to choose if memory should be initialized with binary file or left filled with ones. Commented out some statements while debugging, it works only if it is instantiated inside top level (as oposed to instantiate it in testbench)

--- async_sram.vhd
+++ async_sram.vhd
@@ -13,7 +13,7 @@ use work.my_types.all;--array32
 ---------------------------------------------------
 
 entity async_sram is
-generic	(DATA_WIDTH: natural; ADDR_WIDTH: natural);--data/address widths in bits
+generic	(INIT: boolean; DATA_WIDTH: natural; ADDR_WIDTH: natural);--data/address widths in bits
 port(	IO:	inout std_logic_vector(DATA_WIDTH-1 downto 0);--data bus
 		ADDR:	in std_logic_vector(ADDR_WIDTH-1 downto 0);
 		WE_n:	in std_logic;--write enable, active low
@@ -28,35 +28,86 @@ end async_sram;
 ---------------------------------------------------
 
 architecture behv of async_sram is
+	type mem is array (natural range <>) of std_logic_vector;--natural has 31 bit, goes from 0 to 2^31-1, at most
+	--function for file reading, assumes little-endian
+	-- N: width of word in BYTES;
+	-- L: desired legnth of array;
+	--Don't constrain the length at the return type declaration.
+	impure function get_slv_array_from_file(N: natural; L: natural; fname: string) return mem is
+		type t_char_file is file of character;
+		subtype byte_t is natural range 0 to 255;
+		type byte_arr is array (N-1 downto 0) of byte_t;
+		
+		file F: t_char_file;
+		variable result: mem(0 to L-1)(8*N-1 downto 0) := (others=>(others=>'0'));
+		variable idx: integer;
+		variable char_v : character;
+		variable byte_v : byte_t;
+		variable word_value_v:  natural;
+	begin
+		file_open(F, fname);
+		idx := 0;-- index word being accessed
+		while ((idx < L) and (not endfile(F))) loop
+			for i in 0 to N-1 loop--iterates over bytes of a single word
+				read(F, char_v);
+				byte_v := character'pos(char_v);
+				word_value_v := word_value_v + (byte_v*(2**(8*i)));
+			end loop;
+			
+			result(idx) := std_logic_vector(to_unsigned(word_value_v,8*N));
+			word_value_v := 0;
+			idx := idx+1;
+		end loop;
+		
+		assert (idx < L) report "Binary file does not have enough elements to fill the signal." severity Warning;
+		
+		file_close(F);
+		return result;		 
+	end;
+	
+	function set_initial_value(INIT: boolean; default_value: mem; constant_from_file: mem) return mem is
+	begin
+		if(INIT)then
+			return constant_from_file;
+		else
+			return default_value;
+		end if;
+	end;
 
 	--array of latches
---	type word_t is std_logic_vector(DATA_WIDTH-1 downto 0);
-	type mem is array (0 to 2**ADDR_WIDTH-1) of std_logic_vector(DATA_WIDTH-1 downto 0);
-	signal sram: mem := (others=>(others=>'1'));
+	constant ADDR_WIDTH_implemented: natural := 9;
+	constant initial_value: mem(0 to 2**ADDR_WIDTH_implemented-1)(DATA_WIDTH-1 downto 0):= set_initial_value(INIT,(2**ADDR_WIDTH_implemented-1 downto 0 => (DATA_WIDTH-1 downto 0 => '1')),
+																										get_slv_array_from_file(DATA_WIDTH/8,2**ADDR_WIDTH_implemented,"../../sram_file_experimental.bin"));
+																										
+	signal sram: mem(0 to 2**ADDR_WIDTH_implemented-1)(DATA_WIDTH-1 downto 0) := initial_value;
+																						
 	signal sram_IO_instantaneous:	std_logic_vector(DATA_WIDTH-1 downto 0);--sram data; without delay
 	constant	sram_delay: time:= 10 ns;
+	signal ADDR_uint: natural;
 	
 begin
+	assert (ADDR_WIDTH >= ADDR_WIDTH_implemented) report "Internal parameter ADDR_WIDTH_implemented must be less than or equal to ADDR_WIDTH." severity Error;
 
 	write_proc:process(IO,WE_n,ADDR)
 	begin
 		if(WE_n='0')then
-			sram(to_integer(unsigned(ADDR))) <= IO;
+			sram(to_integer(unsigned(ADDR(ADDR_WIDTH_implemented-1 downto 0)))) <= IO;
 		end if;
 	end process;
 	
 --	sram_IO_instantaneous <= sram(to_integer(unsigned(ADDR))) when (WE_n ='1') else
 --									 (others=>'Z');
 --	IO <= transport sram_IO_instantaneous after sram_delay;--emulates delay in sram response (less than 10 ns)
-	process(WE_n,sram,ADDR)
-	begin
-		if (WE_n ='1') then
---			IO <= transport sram(to_integer(unsigned(ADDR))) after sram_delay;--emulates delay in sram response (less than 10 ns)
-			IO <= sram(to_integer(unsigned(ADDR)));--no delay
-		else
-			IO <= (others=>'Z');
-		end if;
-	end process;
+--	process(WE_n,sram,ADDR,initial_value)
+--	begin
+--		if (WE_n ='1') then
+----			IO <= transport sram(to_integer(unsigned(ADDR))) after sram_delay;--emulates delay in sram response (less than 10 ns)
+			ADDR_uint <= to_integer(unsigned(ADDR(ADDR_WIDTH_implemented-1 downto 0)));
+			IO <= sram(ADDR_uint);-- when (WE_n ='1') else (others=>'Z');--no delay
+--		else
+--			IO <= (others=>'Z');
+--		end if;
+--	end process;
 
 end behv;
 
