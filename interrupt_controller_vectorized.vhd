c4e4dcf3d12ff8efa799a7e3a4e096726db21be4
RenanPicoli <renanps123@gmail.com>
2022-08-03 17:44:34 -0300

    interrupt_controller_vectorized: modified states, can be idle, pending or started (IRQ sent to cpu). There is a flag 'active' to indicate that its ISR is being executed by cpu. This flag is a function of the state, computed by combinatorial logic. This corrected the problem in two iret instructuions being sent in consecutive cycles IN SIMULATION. Needs to be tested in FPGA

--- interrupt_controller_vectorized.vhd
+++ interrupt_controller_vectorized.vhd
@@ -78,17 +78,18 @@ architecture behv of interrupt_controller_vectorized is
 	signal all_registers_output: array32 (95 downto 0) := (others=>(others=>'0'));
 	signal all_periphs_rden: std_logic_vector(95 downto 0);
 	signal address_decoder_wren: std_logic_vector(95 downto 0);
-	type array3 is array (natural range <>) of std_logic_vector (2 downto 0);
+	type array2 is array (natural range <>) of std_logic_vector (1 downto 0);
 	
 	------------signals for IRQ control-------------------
 	
 	--fsm_state(i): (IRQ_pend(i), IRQ_active(i), IRQ_suspended(i))
-	signal fsm_state:				array3(31 downto 0);-- the state of each interrupt is a std_logic(2 downto 0)
+	signal fsm_state:				array2(31 downto 0);-- the state of each interrupt is a std_logic(1 downto 0)
 	
 	signal IRQ_IN_prev:			std_logic_vector(31 downto 0);--state of IRQ_IN in previous clock cycle
 	signal IRQ_pend:				std_logic_vector(31 downto 0);-- IRQ waiting to be transmitted to CPU
-	signal IRQ_active:			std_logic_vector(31 downto 0);-- all IRQ being SERVICED (includes nested interrupts)
-	signal IRQ_suspended:		std_logic_vector(31 downto 0);-- flag indicating that the IRQx was active, but preemption occurred
+	signal IRQ_active:			std_logic_vector(31 downto 0);-- one-hot of the ISR being executed NOW
+	signal IRQ_started:			std_logic_vector(31 downto 0);-- all IRQ being SERVICED (includes nested interrupts)
+--	signal IRQ_suspended:		std_logic_vector(31 downto 0);-- flag indicating that the IRQx was active, but preemption occurred
 	signal IRQ_status:			std_logic_vector(31 downto 0);-- status register
 	signal IRQ_curr_out:			std_logic_vector(31 downto 0);-- number of IRQ CURRENTLY being serviced
 	signal IRQ_curr_stack_out_oh:std_logic_vector(31 downto 0);-- one-hot of the IRQ CURRENTLY being serviced on top of stack	
@@ -97,6 +98,7 @@ architecture behv of interrupt_controller_vectorized is
 	signal IACK_finished:		std_logic_vector(31 downto 0);-- '1' when IACK_OUT is deasserted
 	signal irq:						std_logic;
 	signal tmp:						std_logic_vector(31 downto 0);
+	signal tmp_IRQ_out:			std_logic_vector(31 downto 0);
 
 	------------signals for interrupt vector-------------------
 	signal vector: array32 (L-1 downto 0);--address of interrupt handler
@@ -106,8 +108,8 @@ architecture behv of interrupt_controller_vectorized is
   -- it is allowed to have IRQ's with the same priority, the first IRQ will be serviced
 	signal priorities:array32 (L-1 downto 0);
 	
-	signal ISR_ADDR_in:		std_logic_vector(31 downto 0);--address of next ISR
-	signal tmp_ISR_ADDR_in:	array32(L-1 downto 0);--address of next ISR
+	signal ISR_ADDR_in:	std_logic_vector(31 downto 0);--address of next ISR
+	signal tmp_ISR_ADDR:	array32(L-1 downto 0);--address of next ISR
 	
 	signal preemption: std_logic_vector(31 downto 0) := (others=>'0');--bit i indicates for IRQi that it was preempted
 	signal preemption_evt: std_logic;--flag indicating that an active IRQ was preempted
@@ -124,16 +126,15 @@ begin
 ---------------------------------- FSM ------------------------------------
 ----------- (separate bits for each state, for clarity sake) --------------
 		irq_fsm_i: for i in 0 to L-1 generate
-			--fsm_state: (IRQ_pend, IRQ_active, IRQ_suspended)
-			IRQ_pend(i)			<= fsm_state(i)(2);
-			IRQ_active(i)		<= fsm_state(i)(1);
-			IRQ_suspended(i)	<= fsm_state(i)(0);
+			--fsm_state: (IRQ_pend, IRQ_started)
+			IRQ_pend(i)			<= fsm_state(i)(1);
+			IRQ_started(i)		<= fsm_state(i)(0);
 			
-			irq_pending: process(RST,preemption,IRQ_IN,IRQ_OUT,IACK_OUT,CLK)
+			irq_pending: process(RST,preemption,IRQ_IN,IRQ_OUT,IACK_OUT,IRQ_active,CLK)
 			begin
 				if(RST='1') then
 					--idle state
-					fsm_state(i)	<= "000";
+					fsm_state(i)	<= "00";
 					
 					--resets previous history
 					IRQ_IN_prev(i)		<= '0';
@@ -143,26 +144,19 @@ begin
 					IRQ_IN_prev(i)		<= IRQ_IN(i);
 					
 					-- idle state
-					if (fsm_state(i)="000")then
+					if (fsm_state(i)="00")then
 						if (IRQ_IN(i)='1' and IRQ_IN_prev(i)='0') then-- capture IRQ_IN rising_edge
-							fsm_state(i) <= "100";--enters in IRQ_pend state
+							fsm_state(i) <= "10";--enters in IRQ_pend state
 						end if;
 					-- IRQ_pend state
-					elsif(fsm_state(i)="100")then
-						if (preemption(i)='0' and IRQ_OUT='1')then-- its IRQ is transmitted
-							fsm_state(i) <= "010";--enters in IRQ_active state
+					elsif(fsm_state(i)="10")then
+						if (preemption(i)='0')then
+							fsm_state(i) <= "01";--enters in IRQ_started state
 						end if;
-					--IRQ_active state
-					elsif(fsm_state(i)="010")then
-						if(IACK_IN='1')then-- its ISR finished, cpu sent IACK
-							fsm_state(i) <= "000";--enters in idle state
-						elsif(preemption(i)='1')then-- preemption occurs
-							fsm_state(i) <= "001";--enters in IRQ_suspended state
-						end if;
-					--IRQ_suspended state
-					elsif(fsm_state(i)="001")then
-						if(preemption(i)='0')then-- preemption ceases
-							fsm_state(i) <= "010";--returns to IRQ_active state
+					--IRQ_started state
+					elsif(fsm_state(i)="01")then
+						if(IACK_IN='1' and IRQ_active(i)='1')then-- its ISR finished, cpu sent IACK
+							fsm_state(i) <= "00";--enters in idle state
 						end if;
 					end if;
 				end if;
@@ -172,33 +166,26 @@ begin
 		--unused bits receive '0'
 		fsm_state(31 downto L)		<= (others => (others => '0'));
 		IRQ_pend(31 downto L)		<= (others => '0');
-		IRQ_active(31 downto L)		<= (others => '0');
-		IRQ_suspended(31 downto L)	<= (others => '0');
+		IRQ_started(31 downto L)	<= (others => '0');
 		IRQ_IN_prev(31 downto L)	<= (others => '0');
 		
-		tmp_preferred(0) <= (0=> '1', others=>'0') when (IRQ_pend(0)='1' or IRQ_active(0)='1' or IRQ_suspended(0)='1') else (others=>'0');
+		tmp_preferred(0) <= (0=> '1', others=>'0') when (IRQ_pend(0)='1' or IRQ_started(0)='1') else (others=>'0');
 		arbiter: for i in 1 to L-1 generate
 			--if two IRQ's of equal priority arrive in the along the same clock cycle, the IRQ of highest index takes precendence
-			tmp_preferred(i) <= (i=> '1', others=>'0') when	((IRQ_pend(i)='1' or IRQ_active(i)='1' or IRQ_suspended(i)='1') and
+			tmp_preferred(i) <= (i=> '1', others=>'0') when	((IRQ_pend(i)='1' or IRQ_started(i)='1') and
 																	(priorities(i) = tmp_highest_priority(L-1))) else
 																	tmp_preferred(i-1);
 		end generate arbiter;
 		
-		tmp_highest_priority(0) <= priorities(0) when (IRQ_pend(0)='1' or IRQ_active(0)='1' or IRQ_suspended(0)='1') else (others=>'1');
+		tmp_highest_priority(0) <= priorities(0) when (IRQ_pend(0)='1' or IRQ_started(0)='1') else (others=>'1');
 		highest_priority: for i in 1 to L-1 generate
 			--updates tmp_higher_priority
-			tmp_highest_priority(i) <= priorities(i) when	((IRQ_pend(i)='1' or IRQ_active(i)='1' or IRQ_suspended(i)='1') and
+			tmp_highest_priority(i) <= priorities(i) when ((IRQ_pend(i)='1' or IRQ_started(i)='1') and
 																	(priorities(i) < tmp_highest_priority(i-1))) else
 																	tmp_highest_priority(i-1);
 		end generate highest_priority;
 		
-		preemption <= (IRQ_pend or IRQ_active or IRQ_suspended) xor tmp_preferred(L-1);
-		
---		tmp_preemption_evt(0) <= '1' when (IRQ_active(0)='1' and preemption(0)='1') else '0';
---		preemption_evt_gen: for i in 1 to L-1 generate
---			tmp_preemption_evt(i) <= '1' when (IRQ_active(i)='1' and preemption(i)='1') else tmp_preemption_evt(i-1);
---		end generate preemption_evt_gen;
---		preemption_evt <= tmp_preemption_evt(L-1);
+		preemption <= (IRQ_pend or IRQ_started) and (not tmp_preferred(L-1));
 			
 		-- AFTER irq_pend_out UPDATE
 		--é necessário que o software zere os bits das IRQ atendidas e
@@ -206,19 +193,38 @@ begin
 		iack_out_write: for i in 0 to L-1 generate
 					IACK_OUT(i) <= IACK_IN and IRQ_active(i);
 		end generate;
+		
+		IRQ_active <= IRQ_started and (not preemption);
 
 		-- AFTER irq_pend_out UPDATE
-		tmp(0) <= IRQ_pend(0) and (not preemption(0));
-		tmp_ISR_ADDR_in(0) <= vector(0) when (IRQ_pend(0)='1' and (preemption(0)='0')) else (others=>'0');
-		
-		irq_out_write: for i in 1 to L-1 generate
-			tmp(i) <= tmp(i-1) or (IRQ_pend(i) and (not preemption(i)));
-			tmp_ISR_ADDR_in(i) <= vector(i) when (IRQ_pend(i)='1' and (preemption(i)='0')) else tmp_ISR_ADDR_in(i-1);
-		end generate;		
+		tmp(0) <= tmp_IRQ_out(0);
+		irq_out_write: for i in 0 to L-1 generate
+			tmp_i: if (i /= 0) generate
+				tmp(i) <= tmp(i-1) or tmp_IRQ_out(i);
+			end generate tmp_i;
+			
+			process(RST,IRQ_IN,CLK,IRQ_started,IRQ_pend,preemption)
+			begin
+				if(RST='1')then
+					tmp_IRQ_out(i) <= '0';
+				elsif(rising_edge(CLK))then
+					if(IRQ_started(i)='1')then
+						tmp_IRQ_out(i) <='0';
+					elsif(IRQ_pend(i)='1' and preemption(i)='0')then
+						tmp_IRQ_out(i) <='1';
+					end if;
+				end if;
+			end process;
+		end generate;
 		IRQ_OUT <= tmp(L-1);
-		ISR_ADDR <= tmp_ISR_ADDR_in(L-1);
+		
+		tmp_ISR_ADDR(0) <= vector(0) when (tmp_IRQ_out(0)='1') else (others=>'0');		
+		ISR_ADDR_write: for i in 1 to L-1 generate
+			tmp_ISR_ADDR(i) <= vector(i) when (tmp_IRQ_out(i)='1') else tmp_ISR_ADDR(i-1);
+		end generate;
+		ISR_ADDR <= tmp_ISR_ADDR(L-1);
 	
-	--DOES NOT CHECK if is a true one-hot, if two bits are high, the MSb takes precendence
+	--DOES NOT CHECK if is a true one-hot, if two bits are high, the MSb will take precendence
 	process(IRQ_active)
 	begin
 		IRQ_curr_out <= (others=>'0');
@@ -228,27 +234,6 @@ begin
 			end if;
 		end loop;
 	end process;
-
---	--useful to determine which ISR is being resumed, for example, when there 2 or more IRQ suspended of same priority
---	-- the last (in time of start) should be resumed
---	irq_stack: stack
---		generic map (L => STACK_LEVELS_LOG2)
---		port map (CLK => CLK,--active edge: rising_edge
---					rst => RST,-- active high asynchronous reset (should be deasserted at rising_edge)
---					--STACK INTERFACE
---					pop => IACK_IN,
---					push => preemption_evt,
---					addsp => '0',
---					imm => (others=>'0'),--imm > 0: deletes vars, imm < 0: reserves space for vars
---					stack_in => IRQ_curr_out,-- word to be pushed
---					sp => open,-- points to last stacked item (address of a 32-bit word)
---					stack_out => IRQ_curr_stack_out_oh,--data retrieved from stack
---					--MEMORY-MAPPED INTERFACE
---					D => (others=>'0'),-- data to be written by memory-mapped interface
---					WREN => '0',--write enable for memory-mapped interface
---					ADDR => (others=>'0'),-- address to be written by memory-mapped interface
---					Q    => open-- data output for memory-mapped interface
---			);
 	
 	-------------vector write----------------------------
 	vector_gen: for i in 0 to L-1 generate
@@ -295,7 +280,7 @@ begin
 
 	all_registers_output(0) <= IRQ_pend;
 	all_registers_output(1) <= IRQ_active;
-	all_registers_output(2) <= IRQ_suspended;
+	all_registers_output(2) <= IRQ_started;
 	all_registers_output(3) <= IRQ_status;
 	all_registers_output(4) <= IRQ_curr_out;
 	
