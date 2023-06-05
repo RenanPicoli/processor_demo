e0de09c322959d2c092e5852daa836caa09d0c8e
RenanPicoli <renanps123@gmail.com>
2023-06-05 12:06:30 -0300

    fpu_inner_product.vhd: added another stage to the pipeline (no timing violations in this peripheral, successfully tested)

--- fpu_inner_product.vhd
+++ fpu_inner_product.vhd
@@ -68,11 +68,12 @@ architecture behv of fpu_inner_product is
 	signal final_sum: std_logic_vector(31 downto 0);--connects feedback and feed forward parts
 	signal prod: array32 (0 to N-1);--results of products
 	signal dff_prod: array32 (0 to N-1);--results of products
-	signal req: std_logic_vector (0 to 2);--req_in is passed between pipeline stages
+	signal req: std_logic_vector (0 to 3);--req_in is passed between pipeline stages
+	signal dff_sum_3: std_logic_vector(31 downto 0);
 
 begin
 												
-	--2nd stage of pipeline
+	--3rd stage of pipeline
 	dff_output: d_flip_flop port map(D => final_sum,
 												RST=> RST,--resets all previous history of input signal
 												ENA=> '1',
@@ -82,13 +83,31 @@ begin
 	
 ---------- feed-forward ( A(i)*B(i) ) adders --------------------------------------------------
 	sum_i: for i in 0 to N-2 generate
-		adder: fpu_adder port map(	A => sum(i+1),--supposed to be normalized
-											B => dff_prod(i),--supposed to be normalized
-											-------NEED ADD FLAGS (overflow, underflow, etc)
-											--overflow:		out std_logic,
-											--underflow:		out std_logic,
-											result => sum(i)
-											);
+		sum_3: if i=N/2-1 generate
+			adder: fpu_adder port map(	A => sum(i+1),--supposed to be normalized
+												B => dff_prod(i),--supposed to be normalized
+												-------NEED ADD FLAGS (overflow, underflow, etc)
+												--overflow:		out std_logic,
+												--underflow:		out std_logic,
+												result => dff_sum_3
+												);
+			--2nd stage of pipeline
+			dff_sum: d_flip_flop port map(	D => dff_sum_3,
+														RST=> RST,--resets all previous history of input signal
+														ENA=> '1',
+														CLK=> CLK,--sampling clock
+														Q=> sum(i)
+														);
+		end generate;
+		sum_others: if i/=N/2-1 generate
+			adder: fpu_adder port map(	A => sum(i+1),--supposed to be normalized
+												B => dff_prod(i),--supposed to be normalized
+												-------NEED ADD FLAGS (overflow, underflow, etc)
+												--overflow:		out std_logic,
+												--underflow:		out std_logic,
+												result => sum(i)
+												);
+		end generate;
 	end generate;
 	final_sum <= sum(0);
 	sum(N-1) <= dff_prod(N-1);
@@ -115,7 +134,7 @@ begin
 
 ------------------------------ ready generation ---------------------------------------------
 -------------------(as many registers as pipeline stages)------------------------------------
-	ready_i: for i in 1 to 2 generate
+	ready_i: for i in 1 to 3 generate
 		process(CLK,RST,req)
 		begin
 			if(RST='1')then
@@ -126,7 +145,7 @@ begin
 		end process;
 	end generate;
 	req(0) <= req_in;
-	req_ready <= req(2);
+	req_ready <= req(3);
 
 end behv;
 
