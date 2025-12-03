55c61294d93a21a2d5cb940a3a077320de7821df
RenanPicoli <renanps123@gmail.com>
2025-12-03 02:59:45 -0300

    top level: using pll_dbg_uproc instead of pll_12MHz to generate VGA PCLK

--- processor_demo.vhd
+++ processor_demo.vhd
@@ -245,7 +245,8 @@ component pll_12MHz
 		inclk0		: in std_logic  := '0';
 		c0			: out std_logic;
 		c1			: out std_logic;
-		c2			: out std_logic
+--		c2			: out std_logic;
+		locked	: out std_logic
 	);
 end component;
 
@@ -274,7 +275,7 @@ component pll_dbg_uproc
 		c1 	: OUT STD_LOGIC ;
 		c2 	: OUT STD_LOGIC ;
 		c3 	: OUT STD_LOGIC ;
---		c4 	: OUT STD_LOGIC ;
+		c4 	: OUT STD_LOGIC ;
 		locked		: OUT STD_LOGIC 
 	);
 END component;
@@ -2173,7 +2174,7 @@ signal sda_dbg_s: natural;--for debug, which statement is driving SDA
 		c1		=> CLK,--produces CLK=4MHz for processor
 		c2		=> sram_CLK,--produces 4x the processor frequency, delayed (for 4MHz uproc, produces 16MHz delayed 31.25 ns)
 		c3		=> sdram_ctrl_clk,--100MHz for SDRAM control and IO
---		c4		=> vga_pclk,--40MHz for VGA pixel clock
+		c4		=> vga_pclk,--25MHz for VGA pixel clock
 		locked=> open
 	);
 
@@ -2184,7 +2185,8 @@ signal sda_dbg_s: natural;--for debug, which statement is driving SDA
 	areset => '0',
 	c0 => CLK12MHz,
 	C1 => clk_uart_8x2400,
-	c2 => vga_pclk--25.175MHz for VGA pixel clock (actually 25.161290MHz)
+--	c2 => vga_pclk--25.175MHz for VGA pixel clock (actually 25.161290MHz)
+	locked => open
 	);
 
 	--produces 44118Hz (fs) and 20 MHz (for BCLK_IN) from 12MHz input
