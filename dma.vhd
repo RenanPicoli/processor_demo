45adf56df7ca3da7728607a143cf5b6a6f8f4292
RenanPicoli <renanps123@gmail.com>
2026-07-03 03:10:00 -0300

    dma.vhd: now suports memory latencies of up to 7 cycles

--- dma.vhd
+++ dma.vhd
@@ -37,7 +37,7 @@ architecture behavior of dma_controller is
     signal num_xfers    : std_logic_vector(31 downto 0);
     signal count     : std_logic_vector(31 downto 0) := (others => '0');
     signal count_del : std_logic_vector(31 downto 0) := (others => '0');--count delayed according source memory latency, incremented when data is latched
-    type count_sr_type is array (0 to 3) of std_logic_vector(31 downto 0);
+    type count_sr_type is array (0 to 7) of std_logic_vector(31 downto 0);
     signal count_sr: count_sr_type := (others => (others => '0'));
     
     -- CR agora tem 32 bits com SINC e DINC
@@ -48,7 +48,7 @@ architecture behavior of dma_controller is
     signal fifo      : fifo_type := (others => (others => '0'));
     signal fifo_head : integer range 0 to FIFO_LEN-1 := 0;
     signal fifo_head_del: integer range 0 to FIFO_LEN-1 := 0;--fifo_head delayed according source memory latency
-    type head_sr_type is array (0 to 3) of integer range 0 to FIFO_LEN-1;
+    type head_sr_type is array (0 to 7) of integer range 0 to FIFO_LEN-1;
     signal fifo_head_sr: head_sr_type := (others => 0);
     signal fifo_tail : integer range 0 to FIFO_LEN-1 := 0;
     signal fifo_count: integer range 0 to FIFO_LEN := 0; -- Capacidade da FIFO = FIFO_LEN palavras
@@ -57,7 +57,7 @@ architecture behavior of dma_controller is
     signal state     : std_logic_vector(1 downto 0) := "00"; -- 00 = Idle, 01 = Reading, 10 = Writing
 	 
 --	 signal	prev_mem_ready: std_logic;
-	 type mem_ready_sr_type is array (0 to 3) of std_logic;
+	 type mem_ready_sr_type is array (0 to 7) of std_logic;
 	 signal mem_ready_sr: mem_ready_sr_type := (others => '0');
 	 signal mem_valid : std_logic;--indicates mem_data_in is valid (still valid after CAS latency clocks after mem_ready is deasserted)
 	 
@@ -86,8 +86,8 @@ begin
 	 -- CR(1): IRQ (finished)
 	 -- CR(2): SINC
 	 -- CR(3): DINC
-	 -- CR(5:4): SRC_LAT (source memory latency in mem_clk cycles)
-	 -- CR(6): AUTOSTART (after the manual start, repeats the transfer forever
+	 -- CR(6:4): SRC_LAT (source memory latency in mem_clk cycles)
+	 -- CR(7): AUTOSTART (after the manual start, repeats the transfer forever
     process (clk, reset, count, num_xfers, fifo_count, iack, irq)
     begin
         if reset = '1' then
@@ -137,15 +137,15 @@ begin
 	end process;
 
     --selects fifo_head value according to source memory latency (only for reading)
-	fifo_head_del <= fifo_head_sr(conv_integer(unsigned(CR(5 downto 4))));
+	fifo_head_del <= fifo_head_sr(conv_integer(unsigned(CR(6 downto 4))));
 	fifo_head_sr(0)<=fifo_head;--no latency added
 	
     --selects count value according to source memory latency (only for reading)
-	count_del <= count_sr(conv_integer(unsigned(CR(5 downto 4))));
+	count_del <= count_sr(conv_integer(unsigned(CR(6 downto 4))));
 	count_sr(0)<=count;--no latency added
 	
 	 --keeps track of which data is valid when reading
-	 mem_valid  <= mem_ready_sr(conv_integer(unsigned(CR(5 downto 4))));
+	 mem_valid  <= mem_ready_sr(conv_integer(unsigned(CR(6 downto 4))));
 	 mem_ready_sr(0) <= mem_ready;--no latency added
     -- Máquina de estados para leitura e escrita usando FIFO
     process (mem_clk, reset, iack, mem_ready)
@@ -153,24 +153,24 @@ begin
         if reset = '1' then
             count     <= (others => '0');
             fifo_head <= 0;
-            fifo_head_sr(1 to 3)	<= (others => 0);
-				count_sr(1 to 3)		<= (others => (others => '0'));
+            fifo_head_sr(1 to 7)	<= (others => 0);
+				count_sr(1 to 7)		<= (others => (others => '0'));
+				mem_ready_sr(1 to 7)	<= (others => '0');
             fifo_tail <= 0;
             fifo_count <= 0;
             fifo_count_reg <= 0;
             state     <= "00";-- IDLE
             irq       <= '0';
-				mem_ready_sr(1 to 3)	<= (others => '0');
         elsif rising_edge(mem_clk) then
 			  if mem_rden = '0' then
-					mem_ready_sr(1 to 3)	<= (others => '0');
+					mem_ready_sr(1 to 7)	<= (others => '0');
 				else
-					mem_ready_sr(1 to 3)	<= mem_ready_sr(0 to 2);
+					mem_ready_sr(1 to 7)	<= mem_ready_sr(0 to 6);
 				end if;
 				
 				if mem_valid  = '1' or mem_ready='1' then
-					fifo_head_sr(1 to 3)	<= fifo_head_sr(0 to 2);
-					count_sr(1 to 3)		<= count_sr(0 to 2);
+					fifo_head_sr(1 to 7)	<= fifo_head_sr(0 to 6);
+					count_sr(1 to 7)		<= count_sr(0 to 6);
 				end if;
 				
             case state is
@@ -238,7 +238,7 @@ begin
                     -- Ao transferir o ultimo item, finaliza OU inicia de novo se AUTOSTART estiver ativo
                     if count = num_xfers and fifo_count = 1 and mem_ready='1' then
                         irq   <= '1';
-								if  CR(6)='1' then
+								if  CR(7)='1' then
 									state <= "01";-- goes back to reading
 								else
 									state <= "00";-- idle
@@ -246,7 +246,7 @@ begin
 								-- get ready for new transfers
 								count     <= (others => '0');
 								fifo_head <= 0;
-								fifo_head_sr(1 to 3)<= (others => 0);
+								fifo_head_sr(1 to 7)<= (others => 0);
 								fifo_tail <= 0;
 								fifo_count <= 0;
                     end if;
